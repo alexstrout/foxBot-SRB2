fcd809bef0b89be02036e197772b17ab13ce92ff
Alex Strout <alex@taraxis.com>
2020-11-02 13:07:16 -0500

    Reorganize into simple vs project
    (mostly to easily pack stuff into a pk3)

---
+++ Packager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace foxBot_SRB2 {
+    class Packager {
+        static void Main(string[] args) {
+            string gamePath = @"D:\Games\Sonic Robo Blast 2\";
+
+            string szPath = @"C:\Program Files\7-Zip\7z.exe";
+            string inPath = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\pk3\*";
+            string outPath = gamePath + @"Addons\Self\VL_foxBot.pk3";
+
+            //Package that archive!
+            File.Delete(outPath);
+            _ = Process.Start(szPath, string.Format("a -tzip \"{0}\" \"{1}\"", outPath, inPath));
+
+            //Run that game!
+            _ = Process.Start(
+                new ProcessStartInfo(gamePath + "srb2win.exe", string.Format("-file \"{0}\"", outPath)) {
+                    WorkingDirectory = gamePath
+                }
+            );
+        }
+    }
+}
