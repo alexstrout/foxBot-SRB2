alexstrout/foxBot-SRB2

commit 5d517a98b8af1b715c5085145f3140aa83799029

Movement / angle cleanup
Also fix Minecart targeting regression from 0db9a4d6

VL_foxBot.lua
@@ -864,9 +864,14 @@ local function ValidTarget(bot, leader, bpx, bpy, target, maxtargetdist, maxtarg
 	)
 		ttype = 1 --Can pull sick jumps for these
 	--Vehicles
-	elseif (target.type == MT_MINECARTSPAWNER
-		or (target.type == MT_ROLLOUTROCK and leader.powers[pw_carry] == CR_ROLLOUT))
-	and not target.tracer --No driver
+	elseif (
+		target.type == MT_MINECARTSPAWNER
+		or (
+			target.type == MT_ROLLOUTROCK
+			and leader.powers[pw_carry] == CR_ROLLOUT
+			and not target.tracer --No driver
+		)
+	)
 	and not bot.powers[pw_carry]
 		ttype = -1
 		maxtargetdist = $ * 2 --Vehicles double-distance! (within searchBlockmap coverage)
@@ -1116,13 +1121,11 @@ local function PreThinkFrameFor(bot)
 	local _2d = twodlevel or (bmo.flags2 & MF2_TWOD)
 	local scale = bmo.scale
 	local touchdist = bmo.radius + pmo.radius
-	if bmo.tracer != pmo.tracer
-		if bmo.tracer
-			touchdist = $ + bmo.tracer.radius
-		end
-		if pmo.tracer
-			touchdist = $ + pmo.tracer.radius
-		end
+	if bmo.tracer
+		touchdist = $ + bmo.tracer.radius
+	end
+	if pmo.tracer
+		touchdist = $ + pmo.tracer.radius
 	end
 
 	--Measurements
@@ -1136,7 +1139,7 @@ local function PreThinkFrameFor(bot)
 	local dist = R_PointToDist2(bmo.x, bmo.y, pmo.x, pmo.y)
 	local zdist = FixedMul(pmo.z - bmo.z, scale * flip)
 	local predictfloor = PredictFloorOrCeilingZ(bmo, 1) * flip
-	local ang = 0 --Filled in later depending on target
+	local ang = bmo.angle --Used for climbing etc.
 	local followmax = touchdist + 1024 * scale --Max follow distance before AI begins to enter "panic" state
 	local followthres = touchdist + 92 * scale --Distance that AI will try to reach
 	local followmin = touchdist + 32 * scale
@@ -1161,7 +1164,6 @@ local function PreThinkFrameFor(bot)
 	local targetdist = CV_AISeekDist.value * scale --Distance to seek enemy targets
 	local minspeed = 8 * scale --Minimum speed to spin or adjust combat jump range
 	local pmag = FixedHypot(pcmd.forwardmove * FRACUNIT, pcmd.sidemove * FRACUNIT)
-	local dmf, dms = 0, 0 --Filled in later depending on target
 	local bmosloped = bmo.standingslope and AbsAngle(bmo.standingslope.zangle) > ANGLE_11hh
 
 	--Are we spectating?
@@ -1340,8 +1342,9 @@ local function PreThinkFrameFor(bot)
 		targetdist = R_PointToDist2(bmo.x, bmo.y, bai.target.x, bai.target.y)
 
 		--Override our movement and heading to intercept
-		dmf, dms = DesiredMove(bmo, bai.target, targetdist, 0, 0, 0, bmom, bmogrounded, isspin, _2d)
-		ang = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, bai.target.x, bai.target.y)
+		cmd.forwardmove, cmd.sidemove =
+			DesiredMove(bmo, bai.target, targetdist, 0, 0, 0, bmom, bmogrounded, isspin, _2d)
+		bmo.angle = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, bai.target.x, bai.target.y)
 	else
 		--Lead target if going super fast (and we're close or target behind us)
 		local leaddist = 0
@@ -1356,8 +1359,9 @@ local function PreThinkFrameFor(bot)
 		end
 
 		--Normal follow movement and heading
-		dmf, dms = DesiredMove(bmo, pmo, dist, followmin, leaddist, pmag, bmom, bmogrounded, isspin, _2d)
-		ang = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, pmo.x, pmo.y)
+		cmd.forwardmove, cmd.sidemove =
+			DesiredMove(bmo, pmo, dist, followmin, leaddist, pmag, bmom, bmogrounded, isspin, _2d)
+		bmo.angle = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, pmo.x, pmo.y)
 	end
 
 	--Waypoint! Attempt to negotiate corners
@@ -1368,18 +1372,22 @@ local function PreThinkFrameFor(bot)
 		elseif R_PointToDist2(bmo.x, bmo.y, bai.waypoint.x, bai.waypoint.y) < touchdist
 			bai.waypoint = DestroyObj(bai.waypoint)
 		elseif not bai.target or bai.target.player --Should be valid per above checks
-			--Dist should be DesiredMove's min speed, since we don't want to slow down as we approach the point
-			dmf, dms = DesiredMove(bmo, bai.waypoint, 32 * FRACUNIT, 0, 0, 0, bmom, bmogrounded, isspin, _2d)
-			ang = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, bai.waypoint.x, bai.waypoint.y)
+			--dist eventually recalculates as a total path length (left partial here for aiming vector)
+			--zdist just gets overwritten so we ascend/descend appropriately
+			dist = R_PointToDist2(bmo.x, bmo.y, bai.waypoint.x, bai.waypoint.y)
 			zdist = FixedMul(bai.waypoint.z - bmo.z, scale * flip)
+
+			--Divert through the waypoint
+			cmd.forwardmove, cmd.sidemove =
+				DesiredMove(bmo, bai.waypoint, dist, 0, 0, 0, bmom, bmogrounded, isspin, _2d)
+			bmo.angle = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, bai.waypoint.x, bai.waypoint.y)
+
+			--Finish the dist calc
+			dist = $ + R_PointToDist2(bai.waypoint.x, bai.waypoint.y, pmo.x, pmo.y)
 		end
 	elseif bai.waypoint
 		bai.waypoint = DestroyObj(bai.waypoint)
 	end
-
-	--Set default move here - only overridden when necessary
-	cmd.forwardmove = dmf
-	cmd.sidemove = dms
 
 	--Check water
 	bai.drowning = 0
@@ -1427,20 +1435,14 @@ local function PreThinkFrameFor(bot)
 		cmd.angleturn = pcmd.angleturn
 	end
 
-	--Orientation (may be overridden later)
-	if not bot.climbing
-	and bot.powers[pw_carry] != CR_MINECART
-	and (
-		bai.target
-		or dist > followthres
-		or not (bot.pflags & PF_GLIDING)
-	)
-		bmo.angle = ang
-	end
-
 	--Being carried?
 	if bot.powers[pw_carry]
 		bot.pflags = $ | PF_DIRECTIONCHAR --This just looks nicer
+
+		--Override orientation on minecart
+		if bot.powers[pw_carry] == CR_MINECART and bmo.tracer
+			bmo.angle = bmo.tracer.angle
+		end
 
 		--Jump for targets!
 		if bai.target and bai.target.valid and not bai.target.player
@@ -1566,9 +1568,11 @@ local function PreThinkFrameFor(bot)
 		--Spindash
 		if leader.dashspeed > leader.maxdash / 4
 			if dist > touchdist --Do positioning
-				--This feels dirty, d'oh - same as our normal follow DesiredMove but w/ smaller mindist and no leaddist / minmag
-				cmd.forwardmove, cmd.sidemove = DesiredMove(bmo, pmo, dist, touchdist / 2, 0, 0, bmom, bmogrounded, isspin, _2d)
+				--Same as our normal follow DesiredMove but w/ no mindist / leaddist / minmag
+				cmd.forwardmove, cmd.sidemove =
+					DesiredMove(bmo, pmo, dist, 0, 0, 0, bmom, bmogrounded, isspin, _2d)
 			else
+				bot.pflags = $ | PF_AUTOBRAKE
 				bmo.angle = pmo.angle
 				dodash = 1
 			end
@@ -1690,13 +1694,13 @@ local function PreThinkFrameFor(bot)
 			cmd.sidemove = 0
 			if idle & b1 == b1
 				cmd.forwardmove = 35
-				bmo.angle = ang + ANGLE_270 * imirror
+				bmo.angle = $ + ANGLE_270 * imirror
 			elseif idle & b2 == b2
 				cmd.forwardmove = 25
-				bmo.angle = ang + ANGLE_67h * imirror
+				bmo.angle = $ + ANGLE_67h * imirror
 			elseif idle & b3 == b3
 				cmd.forwardmove = 15
-				bmo.angle = ang + ANGLE_337h * imirror
+				bmo.angle = $ + ANGLE_337h * imirror
 			else
 				bmo.angle = idle * (ANG1 * imirror / 2)
 			end
@@ -1714,7 +1718,7 @@ local function PreThinkFrameFor(bot)
 			if not bai.drowning
 				bot.pflags = $ | PF_AUTOBRAKE --Hit the brakes!
 			else --Water panic?
-				bmo.angle = ang + ANGLE_45
+				bmo.angle = $ + ANGLE_45
 				cmd.forwardmove = 50
 			end
 		end
@@ -1729,7 +1733,7 @@ local function PreThinkFrameFor(bot)
 		or (stalled and not bmosloped
 			and pmofloor - bmofloor > 24 * scale)
 		or bai.stalltics > TICRATE
-		or (isspin and not isdash
+		or (isspin and not isdash and bmom
 			and AbsAngle(bmomang - bmo.angle) > ANGLE_157h) --Spinning
 		or (bai.predictgap & 3 == 3 --Jumping a gap w/ low floor rel. to leader
 			and not bot.powers[pw_carry]) --Not in carry state
@@ -1835,19 +1839,18 @@ local function PreThinkFrameFor(bot)
 
 	--Climb controls
 	if bot.climbing
+		local dmf = zdist
+		local dms = dist
 		if bai.target
 			dmf = FixedMul(bai.target.z - bmo.z, scale * flip)
 			dms = targetdist
-		else
-			dmf = zdist
-			dms = dist
 		end
 		--Don't wiggle around if target's off the wall
 		if AbsAngle(bmo.angle - ang) < ANGLE_67h
 		or AbsAngle(bmo.angle - ang) > ANGLE_112h
 			dms = 0
 		--Shorthand for relative angles >= 180 - meaning, move left
-		elseif bmo.angle - ang < 0
+		elseif ang - bmo.angle < 0
 			dms = -$
 		end
 		if FixedHypot(abs(dmf), abs(dms)) > touchdist
@@ -1861,6 +1864,9 @@ local function PreThinkFrameFor(bot)
 		and (dist > followthres * 2 or zdist < -jumpheight)
 			doabil = -1
 		end
+
+		--Hold our previous angle when climbing
+		bmo.angle = ang
 	end
 
 	--Emergency obstacle evasion!
@@ -2008,7 +2014,7 @@ local function PreThinkFrameFor(bot)
 		end
 
 		--Range modification if momentum in right direction
-		if AbsAngle(bmomang - bmo.angle) < ANGLE_22h
+		if bmom and AbsAngle(bmomang - bmo.angle) < ANGLE_22h
 			mindist = $ + bmom * 8
 
 			--Jump attack should be further timed relative to movespeed
@@ -2145,7 +2151,7 @@ local function PreThinkFrameFor(bot)
 	if isjump and falling
 	and not (doabil or isabil)
 	and (bot.powers[pw_shield] & SH_FORCE)
-	and AbsAngle(bmomang - bmo.angle) > ANGLE_157h
+	and bmom and AbsAngle(bmomang - bmo.angle) > ANGLE_157h
 		dodash = 1
 	end
 

commit a96470aa3a402b65713ca3bfce59f80d4f7e146e

RC2 session fixes
Fix listbots not displaying disconnecting status on bots
Set COM_LOCAL on commands where appropriate
Remove silly timeseed message from setbot
Add debug_botaidump to just dump all AI object info
Average teleport momentum better
Set leader angle on teleport
Fix silly infinite-teleport bug related to leader chains
Reset vertical aiming when assuming control of bot
Fix waypoints sometimes getting rapidly created and destroyed
Fix flymode orientation regression from 5d517a98
Relax targetcount requirements for shield nuke
Fix bot HUD drawing behind PF_FINISHED HUD text

VL_foxBot.lua
@@ -8,7 +8,6 @@
 
 	Future TODO?
 	* Use AdjustedZ in any relative z comparison (maybe just cache like bmofloor etc.)
-	* Add distseed for jump height checks, recalced every jump? So it's not quite so lockstep
 
 	--------------------------------------------------------------------------------
 	Copyright (c) 2020 Alex Strout and CobaltBW
@@ -450,13 +449,14 @@ local function SubListBots(player, leader, bot, level)
 		if bot.ai.ronin
 			msg = $ .. " \x83(disconnected)"
 		end
-	elseif bot.quittime
-		msg = $ .. " \x86(disconnecting)"
 	else
 		msg = $ .. " \x84(player)"
 	end
 	if bot.spectator
 		msg = $ .. " \x87(KO'd)"
+	end
+	if bot.quittime
+		msg = $ .. " \x86(disconnecting)"
 	end
 	CONS_Printf(player, msg)
 	local count = 1
@@ -482,7 +482,7 @@ local function ListBots(player, leader)
 	end
 	CONS_Printf(player, "Returned " .. count .. " nodes")
 end
-COM_AddCommand("LISTBOTS", ListBots, 0)
+COM_AddCommand("LISTBOTS", ListBots, COM_LOCAL)
 
 --Set player as a bot following a particular leader
 --Internal/Admin-only: Optionally specify some other player/bot to follow leader
@@ -509,9 +509,9 @@ local function SetBot(player, leader, bot)
 	--Set up our AI (if needed) and figure out leader
 	SetupAI(pbot)
 	if pleader and pleader.valid
-		CONS_Printf(player, "Set bot " + pbot.name + " following " + pleader.name + " with timeseed " + pbot.ai.timeseed)
+		CONS_Printf(player, "Set bot " + pbot.name + " following " + pleader.name)
 		if player != pbot
-			CONS_Printf(pbot, player.name + " set bot " + pbot.name + " following " + pleader.name + " with timeseed " + pbot.ai.timeseed)
+			CONS_Printf(pbot, player.name + " set bot " + pbot.name + " following " + pleader.name)
 		end
 	elseif pbot.ai.realleader
 		CONS_Printf(player, "Stopping bot " + pbot.name)
@@ -594,6 +594,17 @@ COM_AddCommand("DEBUG_BOTSHIELD", function(player, bot, shield, inv, spd, super,
 	end
 	print(msg)
 end, COM_ADMIN)
+
+--Debug command for printing out AI objects
+COM_AddCommand("DEBUG_BOTAIDUMP", function(player, bot)
+	bot = ResolvePlayerByNum(bot)
+	if not (bot and bot.valid and bot.ai)
+		return
+	end
+	for k, v in pairs(bot.ai)
+		CONS_Printf(player, k .. " = " .. tostring(v))
+	end
+end, COM_LOCAL)
 
 
 
@@ -679,10 +690,10 @@ local function Teleport(bot, fadeout)
 	bmo.state = S_PLAY_JUMP --Looks/feels nicer
 	bot.pflags = $ | P_GetJumpFlags(bot)
 
-	--Average our momentum w/ leader's
-	bmo.momx = ($ - pmo.momx) / 2 + pmo.momx
-	bmo.momy = ($ - pmo.momy) / 2 + pmo.momy
-	bmo.momz = ($ - pmo.momz) / 2 + pmo.momz
+	--Average our momentum w/ leader's - 1/4 ours, 3/4 theirs
+	bmo.momx = $ / 4 + pmo.momx * 3/4
+	bmo.momy = $ / 4 + pmo.momy * 3/4
+	bmo.momz = $ / 4 + pmo.momz * 3/4
 
 	--Zero momy in 2D mode (oops)
 	if bmo.flags2 & MF2_TWOD
@@ -692,6 +703,7 @@ local function Teleport(bot, fadeout)
 	P_TeleportMove(bmo, pmo.x, pmo.y, z)
 	P_SetScale(bmo, pmo.scale)
 	bmo.destscale = pmo.destscale
+	bmo.angle = pmo.angle
 
 	--Fade in (if needed)
 	if bot.powers[pw_flashing] < TICRATE / 2
@@ -1123,6 +1135,7 @@ local function PreThinkFrameFor(bot)
 	if bai.doteleport and Teleport(bot, true)
 		--Post-teleport cleanup
 		bai.doteleport = false
+		bai.playernosight = 0
 		bai.panicjumps = 0
 	end
 
@@ -1136,6 +1149,9 @@ local function PreThinkFrameFor(bot)
 	)
 		if not bai.cmd_time
 			Repossess(bot)
+
+			--Reset our vertical aiming (in case we have vert look disabled)
+			bot.aiming = 0
 
 			--Unset ronin as client must have reconnected
 			--(unfortunately PlayerJoin does not fire for rejoins)
@@ -1389,8 +1405,6 @@ local function PreThinkFrameFor(bot)
 			bai.waypoint = P_SpawnMobj(pmo.x - pmo.momx, pmo.y - pmo.momy, pmo.z - pmo.momz, MT_FOXAI_POINT)
 			--bai.waypoint.state = S_LOCKON3
 			bai.waypoint.ai_type = 1
-		elseif R_PointToDist2(bmo.x, bmo.y, bai.waypoint.x, bai.waypoint.y) < touchdist
-			bai.waypoint = DestroyObj(bai.waypoint)
 		end
 	elseif bai.waypoint
 		bai.waypoint = DestroyObj(bai.waypoint)
@@ -1415,7 +1429,7 @@ local function PreThinkFrameFor(bot)
 		bot.aiming = R_PointToAngle2(0, bmo.z - bmo.momz + bmo.height / 2,
 			targetdist + 32 * FRACUNIT, bai.target.z + bai.target.height / 2)
 	--Waypoint!
-	elseif bai.waypoint
+	elseif bai.waypoint and bai.waypoint.ai_type
 		--Check waypoint sight
 		if CheckSight(bmo, bai.waypoint)
 			bai.targetnosight = 0
@@ -1434,6 +1448,12 @@ local function PreThinkFrameFor(bot)
 		bmo.angle = R_PointToAngle2(bmo.x - bmo.momx, bmo.y - bmo.momy, bai.waypoint.x, bai.waypoint.y)
 		bot.aiming = R_PointToAngle2(0, bmo.z - bmo.momz + bmo.height / 2,
 			dist + 32 * FRACUNIT, bai.waypoint.z + bai.waypoint.height / 2)
+
+		--Check distance to waypoint, disabling if we've reached it
+		--(We don't actually destroy it until we've seen leader again)
+		if dist < touchdist
+			bai.waypoint.ai_type = 0
+		end
 
 		--Finish the dist calc
 		dist = $ + R_PointToDist2(bai.waypoint.x, bai.waypoint.y, pmo.x, pmo.y)
@@ -1505,6 +1525,7 @@ local function PreThinkFrameFor(bot)
 	--Carry pre-orientation (to avoid snapping leader's camera around)
 	if (bot.pflags & PF_CANCARRY) and dist < touchdist * 2
 		cmd.angleturn = pcmd.angleturn
+		bmo.angle = pmo.angle
 	end
 
 	--Being carried?
@@ -1614,6 +1635,8 @@ local function PreThinkFrameFor(bot)
 			or pmo.momz * flip < 0
 				doabil = 1
 			end
+			bmo.angle = pmo.angle
+
 			--Abort if player moves away or spins
 			if --[[dist > touchdist or]] leader.dashspeed > 0
 				bai.flymode = 0
@@ -1951,7 +1974,8 @@ local function PreThinkFrameFor(bot)
 	end
 
 	--Emergency obstacle evasion!
-	if bai.waypoint and bai.targetnosight > TICRATE
+	if bai.waypoint and bai.waypoint.ai_type
+	and bai.targetnosight > TICRATE
 		if BotTime(bai, 2, 4)
 			cmd.sidemove = 50
 		else
@@ -1991,7 +2015,7 @@ local function PreThinkFrameFor(bot)
 		local attack = 0
 		local attshield = (bai.target.flags & (MF_BOSS | MF_ENEMY))
 			and (bot.powers[pw_shield] == SH_ATTRACT
-				or (bot.powers[pw_shield] == SH_ARMAGEDDON and bai.targetcount > 5))
+				or (bot.powers[pw_shield] == SH_ARMAGEDDON and bai.targetcount > 4))
 		--Helpmode!
 		if bai.target.player
 			attkey = BT_USE
@@ -2340,6 +2364,7 @@ local function PreThinkFrameFor(bot)
 
 	--Dead! (Overrides other jump actions)
 	if bot.playerstate == PST_DEAD
+		bai.playernosight = 0 --Don't spawn waypoints or try to teleport
 		bai.stalltics = $ + 1
 		cmd.buttons = $ & ~BT_JUMP
 		if leader.playerstate == PST_LIVE
@@ -2453,8 +2478,13 @@ local function PreThinkFrameFor(bot)
 		--Waypoint?
 		if bai.waypoint
 			hudtext[10] = ""
-			hudtext[11] = "\x87" + "waypoint " + string.gsub(tostring(bai.waypoint), "userdata: ", "")
-				+ " " + R_PointToDist2(bmo.x, bmo.y, bai.waypoint.x, bai.waypoint.y) / scale
+			hudtext[11] = "waypoint " + string.gsub(tostring(bai.waypoint), "userdata: ", "")
+			if bai.waypoint.ai_type
+				hudtext[11] = "\x87" + $ + " "
+					+ R_PointToDist2(bmo.x, bmo.y, bai.waypoint.x, bai.waypoint.y) / scale
+			else
+				hudtext[11] = "\x86" + $
+			end
 		end
 	end
 end
@@ -2649,6 +2679,8 @@ hud.add(function(v, stplyr, cam)
 	--Spectating?
 	if stplyr.spectator
 		y = $ + 44
+	elseif stplyr.pflags & PF_FINISHED
+		y = $ + 22
 	end
 
 	--Account for splitscreen