alexstrout/foxBot-SRB2

commit 4d1536eee41be88d0a54b83cb6df24a36cb256b7

Add some safeguards for repeated teleporting

VL_foxBot.lua
@@ -361,6 +361,7 @@ local function ResetAI(ai)
 	ai.pushtics = 0 --Time leader has pushed against something (used to maybe attack it)
 	ai.longjump = false --AI is making a decently sized leap for an enemy
 	ai.doteleport = false --AI is attempting to teleport
+	ai.teleporttime = 0 --Time since AI has first attempted to teleport
 	ai.predictgap = 0 --AI is jumping a gap
 
 	--Destroy any child objects if they're around
@@ -830,7 +831,7 @@ local function Teleport(bot, fadeout)
 	or leader.powers[pw_carry] == CR_ZOOMTUBE
 	or leader.powers[pw_carry] == CR_MINECART
 	or bot.powers[pw_carry] == CR_MINECART
-		return true
+		return false
 	end
 
 	--No fadeouts supported in zoom tube or quittime
@@ -1477,8 +1478,17 @@ local function PreThinkFrameFor(bot)
 	if CheckSight(bmo, pmo)
 		bai.playernosight = 0
 		UpdateLastSeenPos(bai, pmo)
+
+		--Decrement teleporttime if we can see leader
+		bai.teleporttime = max($ - 1, 0)
 	else
 		bai.playernosight = $ + 1
+
+		--Just instakill on too much teleporting if we still can't see leader
+		if bai.teleporttime > 3 * TICRATE
+			bai.teleporttime = 0
+			P_DamageMobj(bmo, nil, nil, 690000, DMG_INSTAKILL)
+		end
 	end
 
 	--Check leader's teleport status
@@ -1491,10 +1501,13 @@ local function PreThinkFrameFor(bot)
 	bai.doteleport = bai.playernosight > 3 * TICRATE
 		or bai.panicjumps > 3
 	if bai.doteleport and Teleport(bot, true)
+		--Increment teleporttime safeguard - will instakill if it gets too high
+		bai.teleporttime = $ + TICRATE
+
 		--Post-teleport cleanup
 		bai.doteleport = false
-		bai.playernosight = 0
-		bai.panicjumps = 0
+		bai.playernosight = TICRATE
+		bai.panicjumps = 1
 		bai.anxiety = 0
 		bai.panic = 0
 	end
@@ -3287,7 +3300,8 @@ local function PreThinkFrameFor(bot)
 		if bai.bored then p2 = $ .. "\x86" .. "bored " end
 		if bai.panic then p2 = $ .. "\x85" .. "panic " + bai.anxiety .. " "
 		elseif bai.anxiety then p2 = $ .. "\x82" .. "anxiety " + bai.anxiety .. " " end
-		if bai.doteleport then p2 = $ .. "\x84" .. "teleport!" end
+		if bai.doteleport then p2 = $ .. "\x84" .. "teleport! " end
+		if bai.teleporttime then p2 = $ .. "\x86" .. "teleporttime " .. bai.teleporttime end
 		--AI States
 		hudtext[1] = p
 		hudtext[2] = p2

commit f483f6cad0bbf4058e54a0ecd7d9f7e7c96cae93

Fix infinite teleport and Fang combat AI issues
(mostly regressions from 4d1536ee and v1.3)
Also clean up changelog

VL_foxBot.lua
@@ -971,10 +971,12 @@ local function Teleport(bot, fadeout)
 	if leader.powers[pw_carry] == CR_NIGHTSMODE
 	or leader.powers[pw_carry] == CR_ZOOMTUBE
 	or leader.powers[pw_carry] == CR_MINECART
-	or (
-		bot.powers[pw_carry] == CR_MINECART
-		and bot.ai.playernosight < 9 * TICRATE
-	)
+		return true
+	end
+
+	--In a minecart?
+	if bot.powers[pw_carry] == CR_MINECART
+	and bot.ai.playernosight < 6 * TICRATE
 		return false
 	end
 
@@ -2958,7 +2960,7 @@ local function PreThinkFrameFor(bot)
 			and bmogrounded and not bai.attackwait
 			and not bai.targetnosight
 				mindist = max($, abs(targetz - bmoz) * 3/2)
-				maxdist = max($ + mindist, 512 * scale)
+				maxdist = max($, 768 * scale) + mindist
 				attkey = BT_USE
 			end
 		--Melee only attacks on ground if it makes sense
@@ -3004,15 +3006,6 @@ local function PreThinkFrameFor(bot)
 		if ability2 == CA2_GUNSLINGER and attkey != BT_USE
 		and not bai.attackwait --Gunslingers get special attackwait behavior
 			ability2 = nil
-		end
-
-		--Make sure we're facing the right way if stand-attacking
-		if attkey == BT_USE and bmogrounded
-		and (ability2 == CA2_GUNSLINGER or ability2 == CA2_MELEE)
-		and AbsAngle(bot.drawangle - bmo.angle) > ANGLE_22h
-			--Should correct us
-			mindist = 0
-			maxdist = 0
 		end
 
 		--Stay engaged if already jumped or spinning
@@ -3082,7 +3075,7 @@ local function PreThinkFrameFor(bot)
 					cmd.sidemove = -30
 				end
 			--Fire!
-			else
+			elseif targetdist < maxdist
 				attack = 1
 
 				--Halt!
@@ -3327,9 +3320,25 @@ local function PreThinkFrameFor(bot)
 					or bot.dashspeed < bot.maxdash / 3
 						dodash = 1
 					end
+				--Make sure we're facing the right way if stand-attacking
+				elseif bmogrounded
+				and (ability2 == CA2_GUNSLINGER or ability2 == CA2_MELEE)
+				and AbsAngle(bot.drawangle - bmo.angle) > ANGLE_45
+				and (
+					ability2 != CA2_MELEE
+					or targetdist > bai.target.radius + bmo.radius + hintdist
+				)
+					--Do nothing
 				else
 					dospin = 1
 					dodash = 1
+
+					--Maybe jump-shot for a bit
+					if ability2 == CA2_GUNSLINGER
+					and BotTime(bai, 4, 48)
+						dojump = 1
+						bai.longjump = 0
+					end
 				end
 			end
 		end