a1abe75cb45db2c01c289fa194d2660b6ed8b813
Alex Strout <alex@taraxis.com>
2023-09-14 17:58:33 -0400

    Fix Pak3 paths

--- Pak3.bat
+++ Pak3.bat
@@ -1,5 +1,5 @@
 set PakName=L_foxBot.pk3
 set SzPath="C:\Program Files\7-Zip\7z.exe"
 
-del %PakName%*
-%SzPath% a -tzip "%PakName%" "%~dp0\pk3\*"
+del "%~dp0\%PakName%*"
+%SzPath% a -tzip "%~dp0\%PakName%" "%~dp0\pk3\*"
