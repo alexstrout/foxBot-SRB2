45ea26d9bb82db6773ada6a4c88b1787a856c9f1
Alex Strout <alex@taraxis.com>
2024-05-15 11:42:12 -0400

    Reorder name / func arguments for AddBotHook

--- pk3/lua/hooks.lua
+++ pk3/lua/hooks.lua
@@ -1,21 +1,21 @@
 local fb = __foxBot_1
 
-local function AddFBHook(type, func, name, order)
-	if not (func and name)
-		print("\x85-- ERROR: foxBot AddHook func or name nil! --")
+local function AddBotHook(type, name, func, order)
+	if not (name and func)
+		print("\x85-- ERROR: foxBot AddHook name or func nil! --")
 		return
 	end
 
 	order = $ or 0
 	func = {
-		func = func,
-		name = string.upper(name),
+		name = name,
+		func = $,
 		order = order
 	}
 
 	type = "_" .. $
 	local f = fb[type]
-	if f == nil
+	if not f
 		fb[type] = func
 	elseif f.order > order
 		func.nextfunc = f
@@ -30,12 +30,12 @@ local function AddFBHook(type, func, name, order)
 	end
 end
 
-function fb.AddAIHook(func, name, order)
-	AddFBHook("ai", func, name, order)
+function fb.AddAIHook(name, func, order)
+	AddBotHook("ai", name, func, order)
 end
 
-function fb.AddGlobHook(func, name, order)
-	AddFBHook("glob", func, name, order)
+function fb.AddGlobHook(name, func, order)
+	AddBotHook("glob", name, func, order)
 end
 
 local function ExecuteFBHook(type, player)
@@ -57,21 +57,21 @@ addHook("PreThinkFrame", function()
 	end
 end)
 
-fb.AddAIHook(function(player)
+fb.AddAIHook("post", function(player)
 	CONS_Printf(player.ai.leader, #player .. " " .. player.cmd.forwardmove)
-end, "post", 1)
+end, 1)
 
-fb.AddAIHook(function(player)
+fb.AddAIHook("asdf", function(player)
 	player.cmd.forwardmove = P_RandomRange(-50, 50)
-end, "asdf")
+end)
 
-fb.AddAIHook(function(player)
+fb.AddAIHook("pre", function(player)
 	CONS_Printf(player.ai.leader, #player .. " " .. player.cmd.forwardmove)
-end, "pre", -1)
+end, -1)
 
-fb.AddGlobHook(function(player)
+fb.AddGlobHook("asdf", function(player)
 	player.cmd.sidemove = P_RandomRange(-50, 50)
-end, "asdf")
+end)
 
 COM_AddCommand("DEBUG_LISTHOOKS", function(player)
 	local function PrintHooks(player, type)
